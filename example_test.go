15823033577419e18870c2b1cc09c6253591ab0c
barry <kooksee@163.com>
2024-06-06 22:30:50 +0800

    fix: 2024-06-06 22:30:50

---
+++ example_test.go
@@ -0,0 +1,14 @@
+package http_rule
+
+import (
+	asserts "github.com/pubgo/funk/assert"
+)
+
+func ExampleRouteTree() {
+	var tree = NewRouteTree()
+	asserts.Exit(tree.Add("get", url1, "get_test"))
+	pp, err := tree.Match("get", "/v1/users/hh/1111/hello/444/555/hhh/6666/messages/nn/ss/mm/ddd/44:change")
+	asserts.Exit(err)
+	asserts.If(pp.Operation != "get_test", "not match")
+	// Output:
+}
